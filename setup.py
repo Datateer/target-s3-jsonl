510ff26fd2dc223d15312de55dc7de423294e838
Adam Roderick <aroder@gmail.com>
2023-07-13 01:16:22 -0600

    fix syntax

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     install_requires=[
         # 'target-core==0.1.0',
-        'target-core @ https://git@gitlab.com/adam614/target-core.git',
+        'target-core @ git+https://git@gitlab.com/adam614/target-core.git',
         'boto3==1.26.82'
     ]
 )
